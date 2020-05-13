0301b89ca8bb676b1e4b02b74c20dd7b719ded1c
Your Name <you@example.com>
2020-05-13 16:56:55 +0300

    first commit

---
+++ scripts/task-1.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// Напиши скрипт, который, для объекта user, последовательно:
+
+// добавляет поле mood со значением 'happy'
+// заменяет значение hobby на 'skydiving'
+// заменяет значение premium на false
+// выводит содержимое объекта user в формате ключ:
+// значение используя Object.keys() и for...of
+
+
+const user = {
+  name: 'Mango',
+  age: 20,
+  hobby: 'html',
+  premium: true,
+};
+
+user.mood = 'happy';
+user.hobby = 'skydiving';
+user.premium = false;
+
+console.log(user);
+
+const keys = Object.keys(user);
+
+console.log(keys);
+
+for (const key of keys) {
+    console.log('key:', user[key]);
+    console.log(key, ':', user[key]);
+}
+
